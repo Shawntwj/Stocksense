b5620c00e24c9fc78bce9bfe7c5cee5fe3c78933
Jayden Ho <jinglong05@hotmail.com>
2022-08-11 00:12:44 +0800

    (feat) extract data
    - create flask app
    - Extract data from, Stocktwits, GoogleNews, newspaper, twitter, reddit
    - add basic health check

---
+++ backend/app.py
@@ -0,0 +1,291 @@
+import time
+import datetime
+from flask import Flask
+from flask_cors import CORS
+import pandas as pd
+
+
+# libraries for Stocktwits
+import requests
+
+# libraries for News
+from GoogleNews import GoogleNews
+from newspaper import Article
+from newspaper import Config
+
+# libraries for Twitter
+import snscrape.modules.twitter as sntwitter
+
+# libraries for Reddit
+from psaw import PushshiftAPI
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def healthCheck():
+    return 200
+
+# Stocktwits scraper
+def first_check(symbol):
+    url = f"https://api.stocktwits.com/api/2/streams/symbol/{symbol}.json?filter=top&limit=20"
+    req = requests.get(url)
+    data_dict = req.json()
+    if data_dict["response"]["status"] == 200:
+        return True
+    return False
+
+# @app.route('/api/stocktwits/<string:symbol>/<string:senti_type>/')
+def getStocktwits(symbol, senti_type):
+    if (first_check(symbol)):
+        data = []
+
+        # end_date = datetime.datetime.today()
+
+        # start_date = (end_date - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+        # start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+        # end_date = datetime.datetime.strptime(end_date.strftime('%Y-%m-%d'), "%Y-%m-%d")
+        start_date = datetime.datetime.now()
+        end_date = start_date - datetime.timedelta(days=1)
+
+        j = 0   # page number
+        postid = ''
+
+        while start_date >= end_date:
+            url = f"https://api.stocktwits.com/api/2/streams/symbol/{symbol}.json?filter=top&limit=40"
+
+            if j > 0:
+                url += postid
+
+            req = requests.get(url)
+            data_dict = req.json()
+
+            if data_dict["response"]["status"] == 200:
+                main_body = data_dict["messages"]
+                for i in range(len(main_body)):
+                    # person_id_data = main_body[i]["id"]
+                    content_data = main_body[i]["body"]
+
+                    content_data = content_data.replace("’", "'")
+                    content_data = content_data.replace("&#39;","'")
+
+                    newdatetime = main_body[i]["created_at"]
+                    formatfrom="%Y-%m-%dT%H:%M:%SZ"
+                    formatto="%a %d %b %Y, %H:%M:%S GMT"
+                    newdatetime = datetime.datetime.strptime(newdatetime,formatfrom).strftime(formatto)
+
+                    time = main_body[i]["created_at"]
+                    timelist = time.split("T")
+
+                    date_created_data = timelist[0]
+
+                    start_date = datetime.datetime.strptime(date_created_data, "%Y-%m-%d")
+
+                    # if (start_date == end_date):
+
+                    # time_created_data = timelist[1][:-1]
+
+                    # username_data = main_body[i]["user"]["username"]
+                    # name_data = main_body[i]["user"]["name"]
+                    # join_date_data = main_body[i]["user"]["join_date"]
+                    # official_data = main_body[i]["user"]["official"]
+                    # followers_data = main_body[i]["user"]["followers"]
+                    # source_data =  main_body[i]["source"]["title"]
+                    # try:
+                    #     likes_data =  main_body[i]["likes"]["total"]
+                    # except:
+                    #     likes_data = 0
+
+                    postid = data_dict["cursor"]["max"]
+                    postid = "&max=" + str(postid)
+
+                    data.append({
+                        # "person_id": person_id_data,
+                        "content": content_data,
+                        "datetime": newdatetime,
+                        # "date_created": date_created_data,
+                        # "time_created": time_created_data,
+                        # "username": username_data,
+                        # "name": name_data,
+                        # "join_date": join_date_data,
+                        # "official": official_data,
+                        # "followers": followers_data,
+                        # "source": source_data,
+                        # "likes": likes_data
+                    })
+
+            j += 1
+
+        return data
+
+# News scraper
+def getArticleSummary(parsed_news, start_date):
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'
+    config = Config()
+    config.browser_user_agent = user_agent
+
+    data=[]
+    for ind in parsed_news:
+        dicti={}
+        article = Article(ind[1],config=config)
+        try:
+            article.download()
+            article.parse()
+            article.nlp()
+
+            # dicti['title']=article.title
+            dicti['content']=article.text
+            # dicti['summary']=article.summary
+            # dicti['link']=ind[1]
+            if article.publish_date == None:
+                dicti['datetime']=start_date
+            else:
+                dicti['datetime']=article.publish_date
+
+            data.append(dicti)
+        except:
+            pass
+
+    return data
+
+def getGoogleNewsLinks(symbol, start_date):
+    parsed_news = []
+
+    end_date = (start_date - datetime.timedelta(days=1)).strftime('%m/%d/%Y')
+    start_date = start_date.strftime('%m/%d/%Y')
+
+    googlenews=GoogleNews(start = end_date, end = start_date) #month/day/year
+    googlenews.search(symbol)
+
+    for i in range(2, 20):
+        googlenews.getpage(i)
+        result=googlenews.result()
+        df=pd.DataFrame(result)
+
+    for ind in df.index:
+        link = df['link'][ind]
+        parsed_news.append([symbol, link])
+
+    return parsed_news
+
+# @app.route('/api/news/<string:symbol>/<string:senti_type>/')
+def getNews(symbol, senti_type):
+    start_date = datetime.datetime.now()
+    parsed_news = getGoogleNewsLinks(symbol, start_date)
+    data = getArticleSummary(parsed_news, start_date)
+    return data
+
+# Twitter scraper
+# @app.route('/api/twitter/<string:symbol>/<string:senti_type>/')
+def getTwitter(symbol, senti_type):
+    """
+    Using TwitterSearchScraper to scrape data and append tweets to list
+    - assumes date format to be YYYY-MM-DD
+    - end date exclusive for twitter
+    - with conditions: min word length = 5, min like = 200, min followers = 50, min retweet = 5
+    """
+    data = []
+    end_date = datetime.datetime.today()
+    start_date = (end_date - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+    end_date = end_date.strftime('%Y-%m-%d')
+
+    items = sntwitter.TwitterSearchScraper(f"{symbol} since:{start_date} until:{end_date}").get_items()
+    for i,tweet in enumerate(items):
+        if len(tweet.content.split())>=5 and tweet.likeCount>=200 and tweet.user.followersCount>=50 and tweet.retweetCount>=5:
+            data.append({
+                # "username": tweet.user.username,
+                "content": tweet.content,
+                "datetime": tweet.date,
+                # "followers": tweet.user.followersCount,
+                # "comments": tweet.replyCount,
+                # "shares": tweet.retweetCount,
+                # "likes": tweet.likeCount
+            })
+
+    return data
+
+# Reddit scraper
+def reddit_sentiment_comment(search, start, end, subreddit):
+    api = PushshiftAPI()
+
+    s = datetime.datetime.strptime(start, '%d/%m/%Y')
+    e = datetime.datetime.strptime(end, '%d/%m/%Y')
+    start_date = time.mktime(datetime.datetime.strptime(start, "%d/%m/%Y").timetuple())
+    end_date = time.mktime(datetime.datetime.strptime(end, "%d/%m/%Y").timetuple())
+    comments = []
+
+    S = api.search_comments(subreddit=subreddit, after=s, before=e)  # Pull posts within date range
+    for comment in S:  # Looping through each post
+        try: # Try/except to catch any erroneous post pulls
+            if comment.body != '[removed]' and comment.body != '[deleted]' and search[0] in comment.body or search[1] in comment.body or search[2] in comment.body or search[3] in comment.body or search[4] in comment.body: # Remove the deleted posts
+                comments.append({
+                    'content':comment.body,
+                    # 'username':comment.author_fullname,
+                    # 'score':comment.score,
+                    # 'comment_id':comment.id,
+                    # 'subreddit':comment.subreddit,
+                    # 'parent_id':comment.parent_id,
+                    'datetime':datetime.datetime.fromtimestamp(comment.created)
+                })  # Retrieve post data and append to dataframe
+        except:
+            continue # Continue loop if error is found
+
+    return comments
+
+def reddit_sentiment_post(search, start, end, subreddit):
+    api = PushshiftAPI()
+    s = datetime.datetime.strptime(start, '%d/%m/%Y')
+    e = datetime.datetime.strptime(end, '%d/%m/%Y')
+    start_date = time.mktime(datetime.datetime.strptime(start, "%d/%m/%Y").timetuple())
+    end_date = time.mktime(datetime.datetime.strptime(end, "%d/%m/%Y").timetuple())
+    posts = []
+
+    S = api.search_submissions(subreddit=subreddit, after=s, before=e) # Pull posts within date range
+
+    for post in S:  # Looping through each post
+        try: # Try/except to catch any erroneous post pulls
+            if post.title != '[removed]' and post.title != '[deleted]'and search[0] in post.title or search[1] in post.title or search[2] in post.title or search[3] in post.title or search[4] in post.title:
+                posts.append({
+                    # 'title':post.title,
+                    # 'score':post.score,
+                    # 'upvote_ratio':post.upvote_ratio,
+                    # 'id':post.id,
+                    # 'subreddit':post.subreddit,
+                    # 'url':post.url,
+                    # 'num_comments':post.num_comments,
+                    'content':post.selftext,
+                    'datetime':datetime.datetime.fromtimestamp(post.created)
+                })  # Retrieve post data and append to dataframe
+        except:
+            continue # Continue loop if error is found
+
+    return posts
+
+def getReddit(redditType, symbol, senti_type):
+    today = datetime.datetime.today()
+    start_date = (today - datetime.timedelta(days=1)).strftime('%d/%m/%Y')
+    end_date = (today + datetime.timedelta(days=1)).strftime('%d/%m/%Y')
+    # """convert date format to %d/%m/%Y"""
+    # start = "/".join(start_date.split("-")[::-1])
+    # end = (datetime.datetime.strptime(start, "%d/%m/%Y") + datetime.timedelta(days=1)).strftime("%d/%m/%Y")
+    sub = "stocks"
+    if redditType == "comment":
+        data = reddit_sentiment_comment(symbol, start_date, end_date, sub)
+    elif  redditType == "post":
+        data = reddit_sentiment_post(symbol, start_date, end_date, sub)
+    return data
+
+# @app.route('/api/reddit:comment/<string:symbol>/<string:senti_type>/')
+def getRedditComments(symbol, senti_type):
+    return getReddit("comment", symbol, senti_type)
+
+# @app.route('/api/reddit:post/<string:symbol>/<string:senti_type>/')
+def getRedditPosts(symbol, senti_type):
+    return getReddit("post", symbol, senti_type)
+
+
+if __name__ == '__main__':
+    # app.debug = True
+    app.run(debug = True, host='0.0.0.0', port=8000)
